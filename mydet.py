8f1312549b493bea67e32d409aa264075eab629b
Elysiaxw <747440657@qq.com>
2025-03-03 15:30:24 +0800

    Update mydet.py

--- mydet.py
+++ mydet.py
@@ -1,10 +1,13 @@
 #from jetson_inference import detectNet
-#from jetson_utils import videoSource, videoOutputimport jetson.inference
+#from jetson_utils import videoSource, videoOutput
+import jetson.inference
 import jetson.utils
+import argparse
 
-image_path="/home/nvidia/jetson-inference/data/images/ap.jpeg"
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)   
-img = jetson.utils.loadImage(image_path) 
-detections = net.Detect(img)
-display.Render(img)
-display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+img_path="/home/nvidia/jetson-inference/data/images/ap.jpeg"
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+img = jetson.utils.loadImage(img_path)
+detections = net.Detection(img)
+#display.Render(img)
+#display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+jetson.utils.saveImage("/home/nvidia/jetson-inference/examples/myimage.jpg",img)
